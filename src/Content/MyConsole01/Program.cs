bc22d325944e38acacc90ac5dd183aa01ca5f58a
shuhei.nishizawa <shuhei.nishizawa@avanade.com>
2021-02-18 09:55:43 +0900

    initial commit

---
+++ src/Content/MyConsole01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyConsole01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from MyConsole01!");
+        }
+    }
+}
